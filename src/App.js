14b9edf23235bfcc0a32fd55527f901323894419
thinction-a <jakuro94@gmail.com>
2021-01-30 13:48:16 +0900

    immer-tutorial-end

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useRef, useCallback, useState } from 'react';
+import produce from 'immer';
 
 const App = () => {
   const nextId = useRef(1);
@@ -11,10 +12,13 @@ const App = () => {
   const onChange = useCallback(
     e => {
       const { name, value } = e.target;
-      setForm({
-        ...form,
-        [name] : [value]
-      });
+      setForm(
+        // ...form,
+        // [name] : [value]
+        produce(draft => {
+          draft[name] = value;
+        })
+      );
     },
     [form]
   );
@@ -30,10 +34,13 @@ const App = () => {
       };
 
       //array에 새 항목 등록
-      setData({
-        ...data,
-        array: data.array.concat(info)
-      });
+      setData(
+        // ...data,
+        // array: data.array.concat(info)
+        produce(draft => {
+          draft.array.push(info);
+        })
+      );
 
       //form 초기화
       setForm({
@@ -46,15 +53,27 @@ const App = () => {
   )
 
   //항목을 삭제하는 함수
+
+  // const onRemove = useCallback(
+  //   id => {
+  //     setData({
+  //       ...data,
+  //       array: data.array.filter(info => info.id !== id)
+  //     });
+  //   },
+  //   [data]
+  // );
+
   const onRemove = useCallback(
     id => {
-      setData({
-        ...data,
-        array: data.array.filter(info => info.id !== id)
-      });
+      setData(
+        produce(draft => {
+          draft.array.splice(draft.array.findIndex(info => info.id === id), 1);
+        })
+      )
     },
     [data]
-  );
+  )
 
   return (
     <div>
